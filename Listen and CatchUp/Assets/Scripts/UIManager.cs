b2a96553b8d51226a5f7259376e1d7aa3f4341df
renanruan <37370013+renanruan@users.noreply.github.com>
2018-08-29 14:38:05 -0300

    Start

---
+++ Listen and CatchUp/Assets/Scripts/UIManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    public Text Leveltext;
+    public Text Clocktext;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetLevel(string s)
+    {
+        Leveltext.text = s;
+    }
+
+    public void SetClock(string s)
+    {
+        Clocktext.text = s;
+    }
+}
