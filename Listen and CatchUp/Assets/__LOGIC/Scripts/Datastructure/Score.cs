90a49c95d2a049c3a91a59f854dac74a04835326
João Luiz Vieira <joao.euu@gmail.com>
2018-10-26 14:56:29 -0300

    Organiza pasta dos scripts

---
+++ Listen and CatchUp/Assets/__LOGIC/Scripts/Datastructure/Score.cs
@@ -0,0 +1,20 @@
+﻿public class Score
+{
+    private int _rightchoices;
+    private int _wrongchoices;
+    private int _timepoints;
+
+    public Score()
+    {
+        _rightchoices = 0;
+        _wrongchoices = 0;
+        _timepoints = 0;
+    }
+
+    public void RightChoice() => _rightchoices++;
+    public void WrongChoice() => _wrongchoices++;
+    public void TimePoints(int p) => _timepoints += p;
+
+    public int Value => _timepoints;
+    public int RightWords => _rightchoices;
+}
